65d8a321e05c0ca30af53cbf5c4b926bb9c911d4
chenbojian <chenbojian@foxmail.com>
2015-07-20 15:23:45 +0800

    add migrate table

---
+++ core/src/main/resources/db/migration/V1__create_multipleselection_table.sql
@@ -0,0 +1,5 @@
+CREATE TABLE multiple_choice (
+id INT NOT NULL auto_increment,
+cotent VARCHAR(300) NOT NULL,
+PRIMARY KEY (id)
+);
---
+++ core/src/main/resources/db/migration/V2__create_multiple_choice_selection_table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE multiple_choice_selection (
+id INT NOT NULL auto_increment,
+multiple_choice_id INT NOT NULL,
+cotent VARCHAR(300),
+is_answer INT,
+description VARCHAR(800),
+PRIMARY KEY (id),
+FOREIGN KEY (multiple_choice_id) REFERENCES multiple_choice(id)
+);
