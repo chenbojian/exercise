153ba347114da7ea2460bc2d5218cc04b6578356
chenbojian <chenbojian@foxmail.com>
2015-07-20 22:13:28 +0800

    more effect

--- web/src/main/webapp/js/app/exerciseApp.js
+++ web/src/main/webapp/js/app/exerciseApp.js
@@ -1,7 +1,23 @@
 var exerciseApp = angular.module("exerciseApp", []);
 
 exerciseApp.controller("multipleChoiceController",function($scope,$http) {
-    $http.get("./api/list").success(function (data) {
-        $scope.multipleChoices = data;
+    $http.get("./api/list").success(function (multipleChoices) {
+
+        var i = 0;
+        $scope.multipleChoice = multipleChoices[i];
+
+        $scope.next_multiple_choice = function () {
+            i++;
+            $scope.multipleChoice = multipleChoices[i];
+            if(i == multipleChoices.length - 1){
+                $("a.btn").text("完成");
+                $scope.isFinish = true;
+            }
+        }
+        $scope.finish = function () {
+            alert("finish");
+        }
     });
+
+
 });
