f324d3f6dad3c25364abb0e1c9320947ad65ad54
chenbojian <chenbojian@foxmail.com>
2015-07-22 13:52:44 +0800

    can post json to controller. conver to object successful.

--- web/src/main/java/com/exercise/web/api/MultipleChoiceApiController.java
+++ web/src/main/java/com/exercise/web/api/MultipleChoiceApiController.java
@@ -1,11 +1,11 @@
 package com.exercise.web.api;
 
 import com.exercise.core.MultipleChoice;
+import com.exercise.core.MultipleChoiceSelection;
 import com.exercise.core.service.MultipleChoiceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -13,7 +13,7 @@ import java.util.List;
  * Created by chenbojian on 7/20/15.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/multiple-choice")
 public class MultipleChoiceApiController {
 
 
@@ -28,5 +28,16 @@ public class MultipleChoiceApiController {
     @ResponseBody
     public List<MultipleChoice> listOfMultipleChoice() {
         return multipleChoiceService.listMultipleChoice();
+    }
+
+    @RequestMapping(value = "/submit", method = RequestMethod.POST)
+    public void addMultipleChoice(@RequestBody MultipleChoice multipleChoice){
+        System.out.println(multipleChoice.getContent()+"----------------");
+        System.out.println(multipleChoice.getMultipleChoiceSelections()+"----------------");
+        System.out.println(multipleChoice.getMultipleChoiceSelections().size()+"----------------");
+        for (MultipleChoiceSelection multipleChoiceSelection : multipleChoice.getMultipleChoiceSelections()) {
+            System.out.println(multipleChoiceSelection.getContent()+"----------------");
+            System.out.println(multipleChoiceSelection.isAnswer()+"----------------");
+        }
     }
 }
--- web/src/main/webapp/js/app/exerciseApp.js
+++ web/src/main/webapp/js/app/exerciseApp.js
@@ -1,8 +1,7 @@
 var exerciseApp = angular.module("exerciseApp", ["ngRoute"]);
 
 exerciseApp.controller("multipleChoiceController", function ($scope, $http) {
-    $http.get("./api/list").success(function (multipleChoices) {
-
+    $http.get("./api/multiple-choice/list").success(function (multipleChoices) {
         var i = 0;
         $scope.multipleChoice = multipleChoices[i];
 
@@ -34,14 +33,47 @@ exerciseApp.controller("multipleChoiceController", function ($scope, $http) {
             if (this.selection.answer) {
                 $("span.glyphicon").attr("class", "glyphicon glyphicon-ok");
                 this.rightOrWrong = "right";
-            }else {
+            } else {
                 $("span.glyphicon").attr("class", "glyphicon glyphicon-remove");
                 this.rightOrWrong = "wrong";
             }
         }
     };
+});
 
+exerciseApp.controller("inputMultipleChoiceController", function ($scope, $http) {
+    var Selection = function () {
+        this.content = "";
+        this.answer = false;
+        this.description = "";
+    };
 
+    $scope.multipleChoice = {
+        content: "",
+        multipleChoiceSelections: [
+            new Selection(),
+            new Selection(),
+            new Selection()
+        ]
+    };
+    $scope.addSelection = function () {
+        $scope.multipleChoice.multipleChoiceSelections.push(new Selection());
+    };
+    $scope.chooseRightAnswer = function ($index) {
+        $scope.multipleChoice.multipleChoiceSelections
+            .forEach(function (value, index) {
+                value.answer = index == $index;
+            });
+    };
+    $scope.submit = function () {
+        $http.post("./api/multiple-choice/submit", $scope.multipleChoice);
+    }
+});
+
+exerciseApp.filter("toABCD", function () {
+    return function (data) {
+        return String.fromCharCode(data + 65);
+    };
 });
 
 exerciseApp.config(["$routeProvider",
@@ -50,6 +82,10 @@ exerciseApp.config(["$routeProvider",
             when('/', {
                 templateUrl: "partials/multiple-choice.html",
                 controller: "multipleChoiceController"
+            }).
+            when('/input-multiple-choice', {
+                templateUrl: "partials/input-multiple-choice-data.html",
+                controller: "inputMultipleChoiceController"
             }).
             otherwise({
                 redirectTo: "/"
